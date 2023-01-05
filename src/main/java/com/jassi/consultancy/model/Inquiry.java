e3649e689875b7d47713cb915b3353f37c5749c8
sallanjaspreet <jaspreetk767@gmail.com>
2023-01-04 21:40:14 -0500

    Uploaded all changes

---
+++ src/main/java/com/jassi/consultancy/model/Inquiry.java
@@ -0,0 +1,23 @@
+package com.jassi.consultancy.model;
+
+public class Inquiry {
+
+    private String searchString;
+
+    public Inquiry() {
+
+
+    }
+
+    public Inquiry(String searchString) {
+        this.searchString = searchString;
+    }
+
+    public String getSearchString() {
+        return searchString;
+    }
+
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+    }
+}
